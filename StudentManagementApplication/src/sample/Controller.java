c30477e8e84c5cd6baed33f39934a06d73171629
pranavj1001 <pranavj1001@gmail.com>
2016-12-19 11:24:58 +0530

    updated Controller.java

--- StudentManagementApplication/src/sample/Controller.java
+++ StudentManagementApplication/src/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.TextField;
 
 public class Controller {
 
+    //set up the variables for the TextFields
     TextField tfID;
     TextField tfFirstName;
     TextField tfLastName;
@@ -13,8 +14,16 @@ public class Controller {
     TextField tfContactNumber;
     TextField tfTotalMarks;
 
+    //set up the variables for the Button
     Button addButton;
 
+    //set up the variables for the TextArea
     TextArea taDisplay;
+
+    //the setOnAction method for the button
+    //ie whenever the button is clicked this method runs
+    public void addTheData(){
+
+    }
 
 }
