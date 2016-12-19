71d346082d00bb706e9a789f619cc11e41349f76
pranavj1001 <pranavj1001@gmail.com>
2016-12-19 11:15:42 +0530

    minor changes

--- StudentManagementApplication/src/sample/Controller.java
+++ StudentManagementApplication/src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    TextField tfID;
+    TextField tfFirstName;
+    TextField tfLastName;
+    TextField tfEmailAddress;
+    TextField tfContactNumber;
+    TextField tfTotalMarks;
+
+    Button addButton;
+
+    TextArea taDisplay;
+
 }
--- StudentManagementApplication/src/sample/Main.java
+++ StudentManagementApplication/src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Student Management Application");
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
 
