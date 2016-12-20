7afe2ea0691cd1651bfd38a603e14454b46f9686
pranavj1001 <pranavj1001@gmail.com>
2016-12-20 13:52:58 +0530

    improved UI

--- BMIApplication/src/sample/Controller.java
+++ BMIApplication/src/sample/Controller.java
@@ -36,47 +36,53 @@ public class Controller {
         height = Double.parseDouble(tfHeight.getText());
         weight = Double.parseDouble(tfWeight.getText());
 
-        bmi = weight / (height * height);
-        bmiResult = String.valueOf(bmi);
+        if(tfHeight.getText() != "" && tfWeight.getText() != "") {
 
-        if(bmi <= 15){
-            remark = "You are very severely underweight. Please consult a doctor.";
-            lRemark.setStyle("-fx-text-fill: darkblue;");
-            lBMI.setStyle("-fx-text-fill: darkblue;");
-        }else if(bmi > 15 && bmi <= 16){
-            remark = "You are severely underweight. Please consult a doctor.";
-            lRemark.setStyle("-fx-text-fill: darkblue;");
-            lBMI.setStyle("-fx-text-fill: darkblue;");
-        }else if(bmi > 16 && bmi <= 18.5){
-            remark = "You are underweight. Please gain some weight.";
-            lRemark.setStyle("-fx-text-fill: blue;");
-            lBMI.setStyle("-fx-text-fill: blue;");
-        }else if(bmi > 18.5 && bmi <= 25){
-            remark = "You are healthy! Good work!";
-            lRemark.setStyle("-fx-text-fill: green;");
-            lBMI.setStyle("-fx-text-fill: green;");
-        }else if(bmi > 25 && bmi <= 30){
-            remark = "You are overweight. Please try to reduce your weight.";
-            lRemark.setStyle("-fx-text-fill: red;");
-            lBMI.setStyle("-fx-text-fill: red;");
-        }else if(bmi > 30 && bmi <= 35){
-            remark = "You are moderately obese. Please try to reduce your weight.";
-            lRemark.setStyle("-fx-text-fill: darkred;");
-            lBMI.setStyle("-fx-text-fill: darkred;");
-        }else if(bmi > 35 && bmi <= 40){
-            remark = "You are severely obese.Please consult a doctor.";
-            lRemark.setStyle("-fx-text-fill: darkred;");
-            lBMI.setStyle("-fx-text-fill: darkred;");
-        }else if(bmi > 40){
-            remark = "You are very severely obese. Please consult a doctor.";
-            lRemark.setStyle("-fx-text-fill: darkred;");
-            lBMI.setStyle("-fx-text-fill: darkred;");
-        }else{
-            remark = "There's an error in the system. Please give us time to fix it";
+            bmi = weight / (height * height);
+            bmiResult = String.valueOf(bmi);
+
+            if (bmi <= 15) {
+                remark = "You are very severely underweight. Please consult a doctor.";
+                lRemark.setStyle("-fx-text-fill: darkblue;");
+                lBMI.setStyle("-fx-text-fill: darkblue;");
+            } else if (bmi > 15 && bmi <= 16) {
+                remark = "You are severely underweight. Please consult a doctor.";
+                lRemark.setStyle("-fx-text-fill: darkblue;");
+                lBMI.setStyle("-fx-text-fill: darkblue;");
+            } else if (bmi > 16 && bmi <= 18.5) {
+                remark = "You are underweight. Please gain some weight.";
+                lRemark.setStyle("-fx-text-fill: blue;");
+                lBMI.setStyle("-fx-text-fill: blue;");
+            } else if (bmi > 18.5 && bmi <= 25) {
+                remark = "You are healthy! Good work!";
+                lRemark.setStyle("-fx-text-fill: green;");
+                lBMI.setStyle("-fx-text-fill: green;");
+            } else if (bmi > 25 && bmi <= 30) {
+                remark = "You are overweight. Please try to reduce your weight.";
+                lRemark.setStyle("-fx-text-fill: red;");
+                lBMI.setStyle("-fx-text-fill: red;");
+            } else if (bmi > 30 && bmi <= 35) {
+                remark = "You are moderately obese. Please try to reduce your weight.";
+                lRemark.setStyle("-fx-text-fill: darkred;");
+                lBMI.setStyle("-fx-text-fill: darkred;");
+            } else if (bmi > 35 && bmi <= 40) {
+                remark = "You are severely obese.Please consult a doctor.";
+                lRemark.setStyle("-fx-text-fill: darkred;");
+                lBMI.setStyle("-fx-text-fill: darkred;");
+            } else if (bmi > 40) {
+                remark = "You are very severely obese. Please consult a doctor.";
+                lRemark.setStyle("-fx-text-fill: darkred;");
+                lBMI.setStyle("-fx-text-fill: darkred;");
+            } else {
+                remark = "There's an error in the system. Please give us time to fix it";
+            }
+
+            lBMI.setText(bmiResult);
+            lRemark.setText(remark);
+
+        } else {
+
         }
-
-        lBMI.setText(bmiResult);
-        lRemark.setText(remark);
 
     }
 
