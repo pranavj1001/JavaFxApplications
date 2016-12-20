77e74f4acbfa471260fa1949c85c09e7dc6385f0
pranavj1001 <pranavj1001@gmail.com>
2016-12-20 12:48:01 +0530

    the remark system is now working

--- BMIApplication/src/sample/Controller.java
+++ BMIApplication/src/sample/Controller.java
@@ -28,6 +28,28 @@ public class Controller {
 
         lBMI.setText(String.valueOf(bmi));
 
+        if(bmi <= 15){
+            remark = "You are very severely underweight. Please consult a doctor.";
+        }else if(bmi > 15 && bmi <= 16){
+            remark = "You are severely underweight. Please consult a doctor.";
+        }else if(bmi > 16 && bmi <= 18.5){
+            remark = "You are underweight. Please gain some weight.";
+        }else if(bmi > 18.5 && bmi <= 25){
+            remark = "You are healthy! Good work!";
+        }else if(bmi > 25 && bmi <= 30){
+            remark = "You are overweight. Please try to reduce your weight.";
+        }else if(bmi > 30 && bmi <= 35){
+            remark = "You are moderately obese. Please try to reduce your weight.";
+        }else if(bmi > 35 && bmi <= 40){
+            remark = "You are severely obese.Please consult a doctor.";
+        }else if(bmi > 40){
+            remark = "You are very severely obese. Please consult a doctor.";
+        }else{
+            remark = "There's an error in the system. Please give us time to fix it";
+        }
+
+        lRemark.setText(remark);
+
     }
 
 
