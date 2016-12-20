66cae80540faab09ca32cf59742ba573927a7d2a
pranavj1001 <pranavj1001@gmail.com>
2016-12-20 12:36:23 +0530

    the main logic is ready

--- BMIApplication/src/sample/Controller.java
+++ BMIApplication/src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    //set up the variables of the TextFields
+    @FXML private TextField tfHeight;
+    @FXML private TextField tfWeight;
+
+    //set up the variables of the Labels
+    @FXML private Label lBMI;
+    @FXML private Label lRemark;
+
+    @FXML private void calculateTheBMI(){
+
+        double height;
+        double weight;
+        double bmi;
+        String remark;
+
+        height = Double.parseDouble(tfHeight.getText());
+        weight = Double.parseDouble(tfWeight.getText());
+
+        bmi = weight / (height * height);
+
+        lBMI.setText(String.valueOf(bmi));
+
+    }
+
+
 }
