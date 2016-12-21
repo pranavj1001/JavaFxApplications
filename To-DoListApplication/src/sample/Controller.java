01170eb14820493919f79ff22d1ba86992271505
pranavj1001 <pranavj1001@gmail.com>
2016-12-21 22:41:40 +0530

    minor changes

--- To-DoListApplication/src/sample/Controller.java
+++ To-DoListApplication/src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML private void addTask(){
+
+    }
+
+    @FXML private void clearTask(){
+
+    }
+
+    @FXML private void saveList(){
+
+    }
+
+    @FXML private void clearList(){
+
+    }
+
+    @FXML private void sortList(){
+
+    }
+
 }
