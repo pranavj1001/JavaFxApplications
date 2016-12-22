4b9b0b529614d4782c83d3075d7e61bf8f795506
pranavj1001 <pranavj1001@gmail.com>
2016-12-22 20:07:53 +0530

    updated some files

--- To-DoListApplication/src/sample/Controller.java
+++ To-DoListApplication/src/sample/Controller.java
@@ -1,8 +1,43 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class Controller {
+
+    //set up the variables of the Button
+    @FXML private Button bAddTask;
+    @FXML private Button bClearTask;
+    @FXML private Button bSaveList;
+    @FXML private Button bClearList;
+    @FXML private Button bSortList;
+
+    //set up the variables of the Label
+    @FXML private Label lError;
+
+    //set up the variables of the Text Field
+    @FXML private TextField tfTime;
+
+    //set up the variables of the Text Field
+    @FXML private TextArea taTask;
+    @FXML private TextArea taPreview;
+
+    //set up the variables of the Radio Buttons
+    @FXML private RadioButton radioButton1;
+    @FXML private RadioButton radioButton2;
+    @FXML private RadioButton radioButton3;
+    @FXML private RadioButton radioButton4;
+    @FXML private RadioButton radioButton5;
+
+    //set up the variables of the ToggleGroup
+    @FXML private ToggleGroup priority;
+
+    //set up the variables that we need to run the program;
+    private boolean[] steps = new boolean[5];
+    String task = "";
+    String preview = "";
 
     @FXML private void addTask(){
 
