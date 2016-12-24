6d8590f4be155caa0fdc5a5f6c72f7a565b8ca32
pranavj1001 <pranavj1001@gmail.com>
2016-12-24 10:24:04 +0530

    update Controller.java

--- CalculatorApplication/src/sample/Controller.java
+++ CalculatorApplication/src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    //set up the variables of the text
+    @FXML private Text screen;
+
+    //set up the variables
+    private double number = 0;
+    private String operator = "";
+    private boolean start = true;
+
+    @FXML private void processNumber(ActionEvent actionEvent){
+
+    }
+
+    @FXML private void processOperator(){
+
+    }
+
+    @FXML private void calculateAnswer(){
+
+    }
+
+
 }
