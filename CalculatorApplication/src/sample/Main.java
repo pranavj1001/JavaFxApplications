0eeb258a474f9548f0337cac20e23dba8f58c09f
pranavj1001 <pranavj1001@gmail.com>
2016-12-24 09:55:51 +0530

    minor changes

--- CalculatorApplication/src/sample/Main.java
+++ CalculatorApplication/src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(new Scene(root, 300, 465));
         primaryStage.show();
     }
 
